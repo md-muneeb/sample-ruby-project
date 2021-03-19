796d75a39a65259ba6411d83f429a72a303a41a4
Mohammad Muneeb <mmuneeb@catalyte.io>
2021-03-19 10:52:39 -0500

    moved to styles to css file and added validations to fish model

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,3 +1,6 @@
 class Fish < ApplicationRecord
     has_many :items
+
+    validates :name, presence: true, length: { minimum: 1, maximum: 20 }
+    validates :age, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
 end

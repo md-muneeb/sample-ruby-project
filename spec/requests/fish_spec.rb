f8994f2e4544fd81de496698193459da7a308745
Mohammad Muneeb <mmuneeb@catalyte.io>
2021-03-22 10:33:11 -0500

    started writing item tests

--- spec/requests/fish_spec.rb
+++ spec/requests/fish_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Fish", :type => :request do
-    it "returns all fish" do
+    it "return all fish" do
         get fishes_path
         expect(response).to have_http_status(:success)
     end
--- spec/requests/item_spec.rb
+++ spec/requests/item_spec.rb
@@ -1,16 +1,42 @@
 require "rails_helper"
 
 RSpec.describe "Item", :type => :request do
+    it "return all items" do
+        get items_path
+        expect(response).to have_http_status(:success)
+    end
 
     it "successfully creates a new item and redirects to index page" do
-      get new_item_path
-      expect(response).to render_template(:new)
-  
-      post items_path, :params => { :item => { :name => "Bread", :desc => "Grocery", :cost => -4.25, :fish_id => 1 }}
-  
-      expect(response).to redirect_to(items_path)
-      
+        get new_item_path
+        expect(response).to render_template(:new)
+        fish = FactoryBot.create(:fish, name: "Shark", age: 2)
+        post items_path, :params => { :item => { :name => "Bread", :desc => "Grocery", :cost => 2.25, :fish_id => fish.id }}
+        expect(response).to redirect_to(items_path)
     end
-  
-   
-  end
+
+    it "does not creates a new item because of failed validations" do
+        get new_item_path
+        expect(response).to render_template(:new)
+        post items_path, :params => { :item => { :name => "Bread", :desc => "Grocery", :cost => -2.25 }}
+        expect(response).to render_template("new")
+    end
+
+    it "successfully updates an item and redirects to index page" do
+        fish = FactoryBot.create(:fish, name: "Shark", age: 2)
+        item = FactoryBot.create(:item, :name => "Bread", :desc => "Grocery", :cost => 2.25, :fish_id => fish.id )
+        get "/items/#{item.id}/edit"
+        expect(response).to render_template(:edit)
+        put "/items/#{item.id}", :params => { :item => { :name => "Bread", :desc => "Grocery", :cost => 3.25, :fish_id => fish.id }}
+        expect(response).to redirect_to(items_path)
+    end
+
+    it "does not updates an item due to failed validation" do
+        fish = FactoryBot.create(:fish, name: "Shark", age: 2)
+        item = FactoryBot.create(:item, :name => "Milk", :desc => "Dairy", :cost => 2.25, :fish_id => fish.id)
+        get "/items/#{item.id}/edit"
+        expect(response).to render_template(:edit)
+        put "/items/#{item.id}", :params => { :item => { :name => "Milk", :desc => "Dairy", :cost => -2.25, :fish_id => fish.id }}
+        expect(response).to render_template("edit")
+    end
+
+end
